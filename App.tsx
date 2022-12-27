2a2e56effe8e2e1b834752f2f58e94abaa93b7b8
daee <44410043+daee-kang@users.noreply.github.com>
2022-12-26 16:06:57 -0800

    basic auth

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import { Session } from '@supabase/supabase-js';
+import { useState, useEffect } from 'react';
+import { View } from 'react-native';
+
+import { supabase } from './lib/supabase';
+import Account from './screens/Account';
+import Login from './screens/Login';
 
 export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {session && session.user ? <Account key={session.user.id} session={session} /> : <Login />}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
