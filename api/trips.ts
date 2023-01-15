5ea54f80821675693ae2f161586cea02d2a56b1d
daee <44410043+daee-kang@users.noreply.github.com>
2023-01-14 23:23:25 -0800

    feat: fetch and display trips on home page

--- api/trips.ts
+++ api/trips.ts
@@ -1,5 +1,10 @@
 import { supabase } from '../lib/supabase';
 
+export async function getTripsDB(userid: string) {
+  return await supabase.from('trips').select().eq('owner_id', userid);
+}
+export type GetTripsDBResponse = Awaited<ReturnType<typeof getTripsDB>>;
+
 export async function getTripDB(id: string) {
   return await supabase.from('trips').select().eq('id', id);
 }
--- screens/TripsScreen.tsx
+++ screens/TripsScreen.tsx
@@ -1,15 +1,43 @@
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { Button, Text } from '@rneui/themed';
-import { useCallback } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Button, Card, Text } from '@rneui/themed';
+import { useCallback, useState } from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { GetTripDBResponse, getTripsDB } from '../api/trips';
+import useGetSession from '../hooks/useGetSession';
 import { supabase } from '../lib/supabase';
 import { HomeStackParamList } from './HomeScreen';
 
 const TripsScreen = () => {
+  const [trips, setTrips] = useState<GetTripDBResponse['data']>(null);
+  const [fetchingTrips, setFetchingTrips] = useState(false);
+
   const navigation = useNavigation<NativeStackNavigationProp<HomeStackParamList, 'Trips'>>();
+  const { user } = useGetSession();
+
+  const tripFetch = useCallback(async () => {
+    if (fetchingTrips) return;
+
+    setFetchingTrips(true);
+    const { data, error } = await getTripsDB(user.id);
+    if (error) {
+      return console.log(error);
+    }
+    setTrips(
+      data.sort((a, b) => {
+        return new Date(b.created_at).valueOf() - new Date(a.created_at).valueOf();
+      })
+    );
+    setFetchingTrips(false);
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      tripFetch();
+    }, [])
+  );
 
   const onAddTripButtonPress = useCallback(() => {
     navigation.navigate('AddTrip');
@@ -30,6 +58,20 @@ const TripsScreen = () => {
           logout
         </Button>
       </View>
+      {trips &&
+        trips.map((trip) => {
+          return (
+            <TouchableOpacity
+              key={`trip-${trip.id}`}
+              onPress={() => {
+                navigation.navigate('Trip', { id: trip.id });
+              }}>
+              <Card>
+                <Text h4>{trip.name}</Text>
+              </Card>
+            </TouchableOpacity>
+          );
+        })}
     </SafeAreaView>
   );
 };
